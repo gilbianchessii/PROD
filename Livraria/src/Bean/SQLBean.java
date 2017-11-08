382f27d82beda60321dd1383a381aadcae503d85
Yan <yanric2000@hotmail.com>
2017-11-08 19:40:34 -0300

    Criacao dos geters e seters

---
+++ Livraria/src/Bean/SQLBean.java
@@ -0,0 +1,72 @@
+package Bean;
+
+
+public class SQLBean {
+    
+    private int id_estudante; 
+    private String nome;
+    private String pai;
+    private String curso;
+    private String ramo;
+    private int ano;
+    private int semestre;
+
+    public int getId_estudante() {
+        return id_estudante;
+    }
+
+    public void setId_estudante(int id_estudante) {
+        this.id_estudante = id_estudante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPai() {
+        return pai;
+    }
+
+    public void setPai(String pai) {
+        this.pai = pai;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getRamo() {
+        return ramo;
+    }
+
+    public void setRamo(String ramo) {
+        this.ramo = ramo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
+        
+    
+}
